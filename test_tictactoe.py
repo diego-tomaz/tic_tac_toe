a4316db377adbd7aededa56bee5cfe71a6831866
diego-tomaz <54359164+diego-tomaz@users.noreply.github.com>
2023-06-02 22:03:14 -0300

    Create test_tictactoe.py

---
+++ test_tictactoe.py
@@ -0,0 +1,29 @@
+# test_tictactoe.py
+
+import unittest
+from tictactoe import TicTacToe
+
+class TestTicTacToe(unittest.TestCase):
+    def test_make_move(self):
+        game = TicTacToe()
+        self.assertTrue(game.make_move(0))
+        self.assertEqual(game.board[0], 'X')
+        self.assertFalse(game.make_move(0))
+
+    def test_check_win(self):
+        game = TicTacToe()
+        game.make_move(0)  # X
+        game.make_move(3)  # O
+        game.make_move(1)  # X
+        game.make_move(4)  # O
+        game.make_move(2)  # X
+        self.assertEqual(game.check_win(), 'X')
+
+    def test_draw(self):
+        game = TicTacToe()
+        for i in range(9):
+            game.make_move(i)
+        self.assertEqual(game.check_win(), 'Draw')
+
+if __name__ == '__main__':
+    unittest.main()
